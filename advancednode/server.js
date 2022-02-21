0b7971ea88d385aa428e98e1a0a0a2faf089785a
Mmackz <matthewmackenz@gmail.com>
2022-02-21 13:54:09 -0800

    ✅ lesson 12

--- advancednode/server.js
+++ advancednode/server.js
@@ -5,6 +5,7 @@ const path = require("path");
 const session = require("express-session");
 const passport = require("passport");
 const mongoDB = require("mongodb");
+const bcrypt = require("bcrypt");
 const myDB = require("./connection");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const LocalStrategy = require("passport-local");
@@ -72,7 +73,7 @@ myDB(async (client) => {
                myDataBase.insertOne(
                   {
                      username: req.body.username,
-                     password: req.body.password
+                     password: bcrypt.hashSync(req.body.password, 12)
                   },
                   (err, doc) => {
                      if (err) res.redirect("/");
@@ -108,7 +109,7 @@ myDB(async (client) => {
          myDataBase.findOne({ username }, (err, user) => {
             console.log(`User ${username} attempted to login.`);
             if (err) return done(err);
-            if (!user || password !== user.password) return done(null, false);
+            if (!user || !bcrypt.compareSync(password, user.password)) return done(null, false);
             return done(null, user);
          });
       })
