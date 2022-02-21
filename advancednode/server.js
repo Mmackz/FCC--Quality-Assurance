ac007cc06bcdb3914d84c3fc96cc60d83ac6d5d8
Mmackz <matthewmackenz@gmail.com>
2022-02-20 19:02:52 -0800

    ✅ lesson 2

--- advancednode/server.js
+++ advancednode/server.js
@@ -1,21 +1,27 @@
-'use strict';
-require('dotenv').config();
-const express = require('express');
-const myDB = require('./connection');
-const fccTesting = require('./freeCodeCamp/fcctesting.js');
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const myDB = require("./connection");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
 
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
+
+// view engine setup
+app.set("views", path.join(__dirname, "views/pug"));
+app.set("view engine", "pug");
+
+app.use("/public", express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route('/').get((req, res) => {
-  res.render('Load your view here');
+app.route("/").get((req, res) => {
+   res.render("index", {title: "Hello", message: "Please login"});
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+   console.log("Listening on port " + PORT);
 });
