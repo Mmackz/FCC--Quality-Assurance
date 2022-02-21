dc53d0a491ef00367d2e4c8465d0e89dbffcb4af
Mmackz <matthewmackenz@gmail.com>
2022-02-20 19:52:06 -0800

    ✅ lesson 3

--- advancednode/server.js
+++ advancednode/server.js
@@ -2,10 +2,14 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
+const session = require("express-session");
+const passport = require("passport");
+const mongoDB = require("mongodb");
 const myDB = require("./connection");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
 
 const app = express();
+const ObjectID = mongoDB.ObjectID;
 
 fccTesting(app); //For FCC testing purposes
 
@@ -17,8 +21,34 @@ app.use("/public", express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// express-session setup
+app.use(
+   session({
+      secret: process.env.SESSION_SECRET,
+      resave: true,
+      saveUninitialized: true,
+      cookie: { secure: false }
+   })
+);
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+   console.log(user)
+   done(null, user._id);
+ });
+ 
+ passport.deserializeUser((id, done) => {
+   // myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+     
+   // });
+   done(null, null);
+ });
+
 app.route("/").get((req, res) => {
-   res.render("index", {title: "Hello", message: "Please login"});
+   res.render("index", { title: "Hello", message: "Please login" });
 });
 
 const PORT = process.env.PORT || 3000;
